d62efbd37ac9130d5e4b04be141c1f6061161541
Hossam Hammady <github@hammady.net>
2018-01-27 13:24:17 +0300

    Add CORS header to allow serving fonts from different origin

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// CORS header to allow serving fonts from a different origin
+app.get('/fonts/*', function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');    
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
